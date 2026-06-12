6a1e3518b95e14b8a3e0a10d4a7819d6317800e7
gioJioh <sjgio0515@naver.com>
2026-06-12 16:51:58 +0900

    init

---
+++ src/main/java/com/back/popspot/PopspotApplication.java
@@ -0,0 +1,13 @@
+package com.back.popspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopspotApplication.class, args);
+	}
+
+}
